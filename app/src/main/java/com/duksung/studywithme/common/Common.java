857ed86a6c49846b8a1d45a2bb609782e1000177
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-03 10:14:40 +0900

    웹뷰 테스트, make study room 마무리(분야 제외)

--- app/src/main/java/com/duksung/studywithme/common/Common.java
+++ app/src/main/java/com/duksung/studywithme/common/Common.java
@@ -1,5 +1,9 @@
 package com.duksung.studywithme.common;
 
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
@@ -11,7 +15,7 @@ import java.util.Arrays;
 public class Common {
     // static final 변수 선언
 
-    public static final String BASE_SERVER_URL = "http://192.168.0.21:3000";  // ip주소
+    public static final String BASE_SERVER_URL = "https://192.168.0.21:3000";  // ip주소
 //    public static final String BASE_SERVER_URL = "http://192.168.214.1:3000";
 
     public static final String SearchRoomURL = "/mainpage/search";      // 방 검색
@@ -24,6 +28,12 @@ public class Common {
             return true;
         }
         return false;
+    }
+
+    /** dp를 px 단위로 **/
+    public static float dipToPixels(Context context, float dipValue) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dipValue, metrics);
     }
 
 }
---
+++ app/src/main/java/com/duksung/studywithme/fragment/dialog/StudyFieldDialog.java
@@ -0,0 +1,20 @@
+package com.duksung.studywithme.fragment.dialog;
+
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.duksung.studywithme.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class StudyFieldDialog extends BottomSheetDialogFragment {
+
+}
