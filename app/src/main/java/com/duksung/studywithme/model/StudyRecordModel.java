20045f89b8eaefcdc6ab26fdc5aad60b6e8b904d
sw0610 <enthdnjs11@gmail.com>
2021-08-17 01:21:16 +0900

    'mypage'

---
+++ app/src/main/java/com/duksung/studywithme/model/StudyRecordModel.java
@@ -0,0 +1,19 @@
+package com.duksung.studywithme.model;
+
+public class StudyRecordModel {
+
+    String title;
+
+    public StudyRecordModel(String title){
+        this.title=title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
